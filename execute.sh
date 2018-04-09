794205613176ad9d8f5450552b28f0507fd04047
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2018-04-09 12:06:14 -0400

    Modify execute script for C.

--- execute.sh
+++ execute.sh
@@ -7,7 +7,7 @@
 #
 # It MUST then
 #   (a) Compile the GENERATED file
-#         i.e. programs/3-semantics+codegen/valid/test.cpp
+#         i.e. programs/3-semantics+codegen/valid/test.c
 #   (b) Execute the compiled code
 #
 # (if no compilation is needed, then only perform step b)
@@ -16,15 +16,14 @@
 #   (a) Output ONLY the execution
 #   (b) Exit with status code 0 for success, not 0 otherwise
 
-# This command wit
 rm ${1%.*}.out 2> /dev/null
 
 # You MUST replace the following line with the command to compile your generated code
 # Note the bash replacement which changes:
-#   programs/3-semantics+codegen/valid/test.min -> programs/3-semantics+codegen/valid/test.cpp
+#   programs/3-semantics+codegen/valid/test.min -> programs/3-semantics+codegen/valid/test.c
 # stdout is redirected to /dev/null
-FILENAME="${1%.*}.cpp"
-g++ --std=c++11 -o ${1%.*}.out $FILENAME > /dev/null
+FILENAME="${1%.*}.c"
+gcc -std=c11 -o ${1%.*}.out $FILENAME > /dev/null
 
 # You MUST replace the following line with the command to execute your compiled code
 # Note the bash replacement which changes:
