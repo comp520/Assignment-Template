fb9a81f1096c4cf9143bd4ea4de453e898ecb8e8
Alexander Krolik <akrolik@users.noreply.github.com>
2020-01-12 19:47:47 -0500

    Add timeout to benchmarks after 2 minutes.

--- programs/3-benchmarks+codegen/valid/verify.sh
+++ programs/3-benchmarks+codegen/valid/verify.sh
@@ -3,21 +3,31 @@
 IFS=
 
 START_TIME=`date +%s.%N`
-USER_OUTPUT=$(./execute.sh $1 2>&1)
+USER_OUTPUT=$(timeout 2m ./execute.sh $1 2>&1)
+EXIT_CODE=${PIPESTATUS[0]}
 END_TIME=`date +%s.%N`
 
-EXIT_CODE=${PIPESTATUS[0]}
-USER_OUTPUT=$(echo "$USER_OUTPUT" | sed -r 's/([+\-][0-9]+\.[0-9]+e[+\-])([0-9])([^0-9]|$)/\100\2\3/g' | sed -r 's/([+\-][0-9]+\.[0-9]+e[+\-])([0-9][0-9])([^0-9]|$)/\10\2\3/g' | sed 's/\r//g' | sed 's/\\/\\\\/')
-EXPECTED_OUTPUT=$(grep "//~" $1 | sed 's/\/\/~//' | sed 's/\r//g' | sed 's/\\/\\\\/')
-EXPECTED_TIME=$(grep "//&" $1 | sed 's/\/\/&//')
+EXEC_TIME=$(echo "$END_TIME - $START_TIME" | bc -l)
+EXPECTED_TIME=$(grep -a "//&" $1 | sed 's/\/\/&//' | sed 's/^/ /')
+
+if [[ $EXIT_CODE == 124 ]]
+then
+	echo ">>> Execution Time"
+	echo " Exceeds ${EXEC_TIME} seconds"
+	echo ">>> Expected Execution Time"
+	echo $EXPECTED_TIME
+	echo -e ">>> \033[0;31m[timeout]\033[0m"
+	exit 1
+fi
+
+USER_OUTPUT=$(echo "$USER_OUTPUT" | sed -r 's/([+\-][0-9]+\.[0-9]+e[+\-])([0-9])([^0-9]|$)/\100\2\3/g' | sed -r 's/([+\-][0-9]+\.[0-9]+e[+\-])([0-9][0-9])([^0-9]|$)/\10\2\3/g' | sed 's/\\/\\\\/')
+EXPECTED_OUTPUT=$(grep -a "//~" $1 | sed 's/\/\/~//' | sed 's/\\/\\\\/')
 
 SHOULD_ERROR=$(grep "//!" $1 | wc -l)
 SEGFAULT=$(echo "$USER_OUTPUT" | grep -i "segmentation" | wc -l) 
 
 if [[ "$USER_OUTPUT" == "$EXPECTED_OUTPUT" || ( $SHOULD_ERROR == "1" && $EXIT_CODE != 0 && $SEGFAULT == 0 ) ]]
 then
-	EXEC_TIME=$(echo "$END_TIME - $START_TIME" | bc -l)
-
 	echo ">>> Execution Time"
 	echo "${EXEC_TIME} seconds"
 	echo ">>> Expected Execution Time"
